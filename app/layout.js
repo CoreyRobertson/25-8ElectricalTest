f90094644d4587253567068cd8b36b80a1712395
coreyr <coreystephenr@gmail.com>
2023-09-12 12:26:58 +1000

    third commit

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,6 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
-import Head from 'next/head'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,31 +15,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 
-  const jsonLD = `
-  {
-    "@context": "http://schema.org",
-    "@type": "Organization",
-    "name": "25/8 Electrical",
-    "address": {
-      "@type": "PostalAddress",
-      "streetAddress": "6/21-23 Levanswell Rd",
-      "addressLocality": "Moorabbin",
-      "addressRegion": "Victoria",
-      "postalCode": "3189"
-    },
-    "telephone": "0466-382-487",
-    "url": "https://www.yourwebsite.com"
-    
-  }
-`;
+
 
   return (
     <html lang="en">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: jsonLD }} />
-      </Head>
       <body className={inter.className}>
         <Navbar />
 
